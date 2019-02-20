3e26e7ff8a284224ded86dc203e1cca726b45a92
mrssallyintown <45050947+mrssallyintown@users.noreply.github.com>
2019-02-20 15:21:43 -0700

    Update GameEntity.java

--- TankRoyale Text Based/src/codes/GameEntity.java
+++ TankRoyale Text Based/src/codes/GameEntity.java
@@ -1,27 +1,48 @@
 package codes;
-
+/**
+ *The base class inherited by all TankRoyale entities. 
+ *@author Team 7
+ *@version 1.0
+ *@since Feb. 19th, 2019
+ */
 public class GameEntity {
-
 	private int x, y;
 	
+	/**
+	*Creates a new GameEntity with coordinates x and y
+	@param int x
+	@param int y
+	*/
 	public GameEntity(int x, int y) {
 		super();
 		this.x = x;
 		this.y = y;
 	}
-
+	
+	/**
+	*@return int x
+	*/
 	public int getX() {
 		return x;
 	}
-
+	
+	/**
+	*sets the x coordinate
+	*/
 	public void setX(int x) {
 		this.x = x;
 	}
-
+	
+	/**
+	*@return int y
+	*/
 	public int getY() {
 		return y;
 	}
-
+	
+	/**
+	*sets the y coordinate
+	*/
 	public void setY(int y) {
 		this.y = y;
 	}
