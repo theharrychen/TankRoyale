a554006c4fe64209c711370c40b3044de7a5e19e
Harry Chen <46468236+theharrychen@users.noreply.github.com>
2019-02-24 16:55:16 -0700

    Josh's comments for Bullet.java added

--- TankRoyale Text Based/src/codes/Bullet.java
+++ TankRoyale Text Based/src/codes/Bullet.java
@@ -1,9 +1,24 @@
 package codes;
 
+/**
+ * This class is used to generate a bullet or project to be shot by the tank.
+ * The class extends GameEntity which tracks the bullets x and y coordinates in
+ * the generated grid/array.
+ * 
+ * @author Team 7
+ * @version 0.1
+ * @since 2019-02-19
+ */
 public class Bullet extends GameEntity {
     
     public static final char symbol = '*';
     
+    /**
+     * This constructor generates an object of type Bullet, creating the character '*' 
+     * at the specified x and y coordinates.
+     * @param x the x coordinate of the bullet in the array/grid.
+     * @param y the y coordinate of the bullet in the array/grid.
+     */
     public Bullet (int x, int y){
         super(x, y);
     }
