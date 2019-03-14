cdbf7b59744c9ee1ed0eb561759542407923f99c
AnjolaAA <45274850+AnjolaAA@users.noreply.github.com>
2019-03-13 22:45:54 -0600

    Update KinematicEntity.java

--- Trial GUI/src/codes/KinematicEntity.java
+++ Trial GUI/src/codes/KinematicEntity.java
@@ -1,9 +1,11 @@
 package codes;
 
 /**
- * This class is used to define the velocities of Tank and Bullet objects in TankRoyale
+ * This class is used to define the velocities of Tank and Bullet objects in TankRoyale and by extension, 
+ any non-Static object.
  * 
- * @author Team 7
+ * @author Group 7, adapted from Almas Baimagambetov: https://www.youtube.com/
+	 watch?v=l2XhUHW8Oa4&list=PLurZmf6mNWh4oNzAph6vk14xj9NdS-RCP&index=2&t=0s
  * @version 1.0
  * @since 2019-02-19
  */
@@ -19,15 +21,20 @@ public class KinematicEntity extends GameEntity {
         super(view);
     }
 
+    //Continually updates the position of the moving object through the Nodial positions 
     public void update() { // Called once per frame
         getView().setTranslateX(getView().getTranslateX() + velocity.getX());
         getView().setTranslateY(getView().getTranslateY() + velocity.getY());
     }
 
+    /**Updates the velocity to new object 
+    @param velocity: new velocity of the Point2D object 
+    */
     public void setVelocity(Point2D velocity) {
         this.velocity = velocity;
     }
 
+   //Returns the velocity of the dynamic object.
     public Point2D getVelocity() {
         return velocity;
     }
