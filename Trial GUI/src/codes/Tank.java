b1e8506648cd348351e7c551e0fe8b1347cbf514
AnjolaAA <45274850+AnjolaAA@users.noreply.github.com>
2019-03-13 22:49:57 -0600

    Update Tank.java

--- Trial GUI/src/codes/Tank.java
+++ Trial GUI/src/codes/Tank.java
@@ -3,7 +3,8 @@ package codes;
 /**
  * This tank class is responsible for implementing the behaviour of a tank object
  * 
- * @author Team 7
+ * @author Group 7, adapted from Almas Baimagambetov: https://www.youtube.com/
+	 watch?v=l2XhUHW8Oa4&list=PLurZmf6mNWh4oNzAph6vk14xj9NdS-RCP&index=2&t=0s
  * @version 1.0
  * @since 2019-03-06
  */
