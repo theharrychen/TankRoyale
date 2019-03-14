031f9928e54abc0a1640d143e4e101e70acfbb1c
AnjolaAA <45274850+AnjolaAA@users.noreply.github.com>
2019-03-13 22:35:12 -0600

    Update GameEntity.java

--- Trial GUI/src/codes/GameEntity.java
+++ Trial GUI/src/codes/GameEntity.java
@@ -14,33 +14,44 @@ import javafx.scene.Node;
 
 public class GameEntity { // By default a physically "static" object
 
-    private Node view;
+    private Node view; //An item in the scene graph 
     private boolean alive = true;
 
+    //Contructor: Creates a GameEntity
     public GameEntity(Node view) {
         setView(view);
     }
 
+    /**Updates the game Node to the new position
+    @param view: intended new view
+    */
     public void setView(Node view) {
         this.view = view;
     }
 
+    //Returns the Node (possible privacy leak)
     public Node getView() {
         return view;
     }
 
+    /**Updates current state to alive.
+    @param alive: new intended state
+    */
     public void setAlive(boolean alive) {
         this.alive = alive;
     }
 
+    //Setting the state to alive
     public boolean isAlive() {
         return alive;
     }
 
+    //Returns false if static is dead 
     public boolean isDead() {
         return !alive;
     }
 
+    //Checks if any two objects are at the same position -> therefore colliding. 
     public boolean isColliding(GameEntity other) {
         return getView().getBoundsInParent().intersects(other.getView().getBoundsInParent());
     }
