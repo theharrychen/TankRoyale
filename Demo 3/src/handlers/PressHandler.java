ec2aa44560c8b30bd35a8475b5b8b5225b0da52b
30019363 <47429114+30019363@users.noreply.github.com>
2019-03-24 09:57:30 -0600

    fixed shooting bug

--- Demo 3/src/handlers/PressHandler.java
+++ Demo 3/src/handlers/PressHandler.java
@@ -1,95 +1,97 @@
 package handlers;
 
 import logic.*;
+import visuals.*;
 
 import javafx.event.EventHandler;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 
-public class PressHandler implements EventHandler<KeyEvent>{
+public class PressHandler extends Game implements EventHandler<KeyEvent>{
 
-    Game game = new Game();
+    private GUI test = new GUI();
+    private Bullet b = new Bullet();
     
-    public void onePlayer(KeyCode key){
-        switch (key) {
+    public void onePlayer(KeyEvent key){
+        switch (key.getCode()) {
 			case UP:
-				game.getTanks().get(0).setUp(true);
+				getTanks().get(0).setUp(true);
 				break;
 			case DOWN:
-				game.getTanks().get(0).setDown(true);
+				getTanks().get(0).setDown(true);
 				break;
 			case LEFT:
-				game.getTanks().get(0).setLeft(true);
+				getTanks().get(0).setLeft(true);
 				break;
 			case RIGHT:
-				game.getTanks().get(0).setRight(true);
+				getTanks().get(0).setRight(true);
 				break;
-			case ENTER:
-				if(game.getTanks().get(0).getShooting() == false){
-					game.shoot(game.getTanks().get(0));
-					game.getTanks().get(0).setShooting(true);
-				}
+            case ENTER:
+				if(getTanks().get(0).getShooting() == false){
+                    setP1Shooting(true);
+                    getTanks().get(0).setShooting(true);
+                }
 				break;
         }
     }
 
-    public void twoPlayer(KeyCode key){
+    public void twoPlayer(KeyEvent key){
         onePlayer(key);
 
-        switch (key) {
+        switch (key.getCode()) {
             case W:
-                game.getTanks().get(1).setUp(true);
+                getTanks().get(1).setUp(true);
                 break;
             case S:
-                game.getTanks().get(1).setDown(true);
+                getTanks().get(1).setDown(true);
                 break;
             case A:
-                game.getTanks().get(1).setLeft(true);
+                getTanks().get(1).setLeft(true);
                 break;
             case D:
-                game.getTanks().get(1).setRight(true);
+                getTanks().get(1).setRight(true);
                 break;
             case Q:
-            if(game.getTanks().get(1).getShooting() == false){
-                game.shoot(game.getTanks().get(1));
-                game.getTanks().get(1).setShooting(true);
+            if(getTanks().get(1).getShooting() == false){
+                setP2Shooting(true);
+                getTanks().get(1).setShooting(true);
             }
             break;
         }
     }
 
-    public void threePlayer(KeyCode key){
+    public void threePlayer(KeyEvent key){
         twoPlayer(key);
 
-        switch (key) {
+        switch (key.getCode()) {
             case Y:
-                game.getTanks().get(2).setUp(true);
+                getTanks().get(2).setUp(true);
                 break;
             case H:
-                game.getTanks().get(2).setDown(true);
+                getTanks().get(2).setDown(true);
                 break;
             case G:
-                game.getTanks().get(2).setLeft(true);
+                getTanks().get(2).setLeft(true);
                 break;
             case J:
-                game.getTanks().get(2).setRight(true);
+                getTanks().get(2).setRight(true);
                 break;
             case SPACE:
-            if(game.getTanks().get(2).getShooting() == false){
-                game.shoot(game.getTanks().get(2));
-                game.getTanks().get(2).setShooting(true);
-            }
+            /*if(getTanks().get(2).getShooting() == false){
+                shoot(getTanks().get(2)); //change to p3Shooting
+                getTanks().get(2).setShooting(true);
+            }*/
             break;
         }
     }
 
     public void handle(KeyEvent key){
-        int players = game.getPlayerCount();
+        int players = getPlayerCount();
         if(players == 1)  
-            onePlayer(key.getCode());
+            onePlayer(key);
         else if(players == 2)
-            twoPlayer(key.getCode());
+            twoPlayer(key);
         else if(players == 3)
-            threePlayer(key.getCode());  
+            threePlayer(key);  
     }
 }
