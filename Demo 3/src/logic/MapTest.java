178dac5fc2cf8c4ad9870aee5c7bd032bd0e92b4
mrssallyintown <45050947+mrssallyintown@users.noreply.github.com>
2019-03-28 04:50:38 -0600

    Add files via upload

--- Demo 3/src/logic/MapTest.java
+++ Demo 3/src/logic/MapTest.java
@@ -1,4 +1,12 @@
 package logic;
+/**
+ * This class is used to test the Map logic class
+ * 
+ * @author Group 7, adapted from Almas Baimagambetov: https://www.youtube.com/
+	 watch?v=l2XhUHW8Oa4&list=PLurZmf6mNWh4oNzAph6vk14xj9NdS-RCP&index=2&t=0s
+ * @version 1.0
+ * @since 2019-03-21
+ */
 
 import static org.junit.Assert.*;
 import java.util.*;
@@ -8,6 +16,9 @@ import org.junit.Test;
 public class MapTest{
 	Map testMap = new Map("/resources/GUI/Test.txt");
 	
+	/**
+	 *Testing the setter method for the Char array
+	 */
 	@Test 
 	public void test_setCharMap(){
 			char [][] testarray;
@@ -16,6 +27,9 @@ public class MapTest{
 			assertArrayEquals("Set Character Map to a new array. Unexpected character returned",testarray, testMap.getCharMap());
 	}
 	
+	/**
+	 *Testing the getter method for the Char array
+	 */
 	@Test 
 	public void test_getCharMap(){
 			char [][] testarray;
@@ -23,6 +37,9 @@ public class MapTest{
 			assertArrayEquals("Unexpected character array returned",testarray, testMap.getCharMap());
 	}
 	
+	/**
+	 *Testing the proper construction of the char array
+	 */
 	@Test 
 	public void test_createdcharMap(){
 			char [][] testarray;
@@ -30,29 +47,34 @@ public class MapTest{
 			assertArrayEquals("Unexpected character array conversion of Map textfile",testarray, testMap.getCharMap());
 	}
 	
-	/**@Test 
-	public void test_invalidfileinput(){
-			Map testinvalidmap = new Map("/nottherightdirectory");
-			assertNull("Invalid file input did not return exception", testinvalidmap.getCharMap());
-	}
-	*/
-	
+	/**
+	 *Testing the getter method for the "height" of the created map 
+	 */
 	@Test 
 	public void test_getHeight(){
 			assertEquals("Unexpected height of charMap returned ",2,testMap.getHeight());
 	}
 	
+	/**
+	 *Testing the setter method for the "height" of the created map
+	 */
 	@Test 
 	public void test_setHeight(){
 			testMap.setHeight(5);
 			assertEquals("Set height of charMap to 5. Unexpected height of charMap returned ",5,testMap.getHeight());
 	}
 	
+	/**
+	 *Testing the getter method for the "width" of the created map 
+	 */
 	@Test 
 	public void test_getWidth(){
 			assertEquals("Unexpected width of charMap returned ",4,testMap.getWidth());
 	}
 	
+	/**
+	 *Testing the setter method for the "width" of the created map 
+	 */
 	@Test 
 	public void test_setWidth(){
 			testMap.setWidth(2);
--- Demo 3/src/logic/WallTest.java
+++ Demo 3/src/logic/WallTest.java
@@ -1,5 +1,14 @@
 package logic;
 
+/**
+ * This class is used to test the Wall logic class
+ * 
+ * @author Group 7, adapted from Almas Baimagambetov: https://www.youtube.com/
+	 watch?v=l2XhUHW8Oa4&list=PLurZmf6mNWh4oNzAph6vk14xj9NdS-RCP&index=2&t=0s
+ * @version 1.0
+ * @since 2019-03-21
+ */
+
 import drivers.*;
 
 import static org.junit.Assert.*;
@@ -11,7 +20,9 @@ import javafx.scene.paint.Color;
 
 public class WallTest{
 	
-	//Tests proper construction of wall object
+	/**
+	 *Testing the constructor of Wall 
+	 */
 	@Test
 	public void test_wallconstructor(){
 			Wall w = new Wall(5,5,0);
@@ -22,26 +33,34 @@ public class WallTest{
 			assertEquals("Unexpected horizontal alignment of created wall",1, w2.getWallAlignment());
 	}
 	
-	//Testing getter and setters
+	/**
+	 *Testing the setter method for wall width 
+	 */
 	@Test
 	public void test_setWallWidth(){
 			Wall w = new Wall(5,5,0);
 			w.setWallWidth(7.0);
 			assertEquals("Set wall width to 7.0. Unexpected wall width value",7.0, w.getWallWidth(),0.001);
-			w.setWallWidth(1080 + 100);
+			w.setWallWidth(MainGUI.WIDTH + 100);
 			assertEquals("Set invalid width of wall. Wall width should be less than the width of the game window",7.0, w.getWallWidth(),0.001);
 			
 	}
 	
+	/**
+	 *Testing the setter method for wall height
+	 */
 	@Test
 	public void test_setWallHeight(){
 		Wall w = new Wall(5,5,0);
 			w.setWallHeight(7.0);
 			assertEquals("Set wall height to 7.0. Unexpected wall height value",7.0, w.getWallHeight(),0.001);
-			w.setWallHeight(1080);
+			w.setWallHeight(MainGUI.HEIGHT);
 			assertEquals("Set invalid height of wall. Wall height should be 100 pixels less than the height of the game window",7.0, w.getWallHeight(),0.001);
 	}
 	
+	/**
+	 *Testing the setter method for wall alignment
+	 */
 	@Test
 	public void test_setAlignment(){
 		Wall w = new Wall(5,5,0);
@@ -53,53 +72,58 @@ public class WallTest{
 			assertEquals("Set invalid wall alignment",0, w.getWallAlignment());
 	}
 	
+	/**
+	 *Testing the getter method for wall width 
+	 */
 	@Test
 	public void test_getWallWidth(){
 		Wall w = new Wall(5,5,0);
 		assertEquals("Unexpected return value for wall width",5, w.getWallWidth(),0.001);
 	}
 	
+	/**
+	 *Testing the getter method for wall height
+	 */
 	@Test
 	public void test_getWallHeight(){
 		Wall w = new Wall(5,5,0);
 		assertEquals("Unexpected return value for wall height",5, w.getWallHeight(),0.001);
 	}
 	
+	/**
+	 *Testing the getter method for wall alignment
+	 */
 	@Test
 	public void test_getAlignment(){
 		Wall w = new Wall(5,5,1);
 		assertEquals("Unexpected return value for wall alignment",1, w.getWallAlignment());
 	}
 	
-	//Tests 
+	/**
+	 *Testing for invalid wall alignment
+	 */
 	@Test
 	public void test_invalidwallalignment(){
 			Wall w = new Wall(5,5,8);
 			assertEquals("Invalid alignment of the wall. Wall alignment must be 0 or 1",0, w.getWallAlignment());
 	}
 	
+	/**
+	 *Testing for invalid wall width
+	 */
 	@Test
 	public void test_invalidwidth(){
-			Wall w = new Wall(1080+ 100,5,8);
+			Wall w = new Wall(MainGUI.WIDTH + 100,5,8);
 			assertEquals("Invalid width of the wall. Width must be less than the game window's width",0, w.getWallWidth(),0.001);
 	}
 	
+	/**
+	 *Testing for invalid wall height
+	 */
 	@Test
 	public void test_invalidheight(){
-			Wall w = new Wall(1080,5,8);
+			Wall w = new Wall(MainGUI.HEIGHT,5,8);
 			assertEquals("Invalid height of the wall. Height must be 100 pixles less than the game window's height",0, w.getWallHeight(),0.001);
 	}
-	
-	/**
-	@Test
-	public void test_checkParameters(){
-		Wall w = new Wall(5,5,0);
-		Rectangle valid = new Rectangle(5, 5, Color.gray(0.5));
-		Rectangle invalid = new Rectangle(0, 0, Color.gray(0.5));
-		assertTrue("Unexpected rectangle returned after checking valid wall constructor parameters", valid.equals(w.checkParameters(5.0,5.0,0)));
-		assertTrue("Unexpected rectangle returned after checking invalid width in wall constructor parameters ", invalid.equals(w.checkParameters(MainGUI.WIDTH + 100,5,0)));
-		assertTrue("Unexpected rectangle returned after checking invalid height in wall constructor parameters", invalid.equals(w.checkParameters(5,MainGUI.HEIGHT,0)));
-		assertTrue("Unexpected rectangle returned after checking invalid alignment wall constructor parameters", invalid.equals(w.checkParameters(5,5,8)));
-	}
-	*/
+
 }
