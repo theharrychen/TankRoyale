a78af41ad8a2480df5c9fb985c20856a1e146b77
mrssallyintown <45050947+mrssallyintown@users.noreply.github.com>
2019-03-28 04:51:21 -0600

    Add files via upload

--- Demo 3/src/visuals/MenuGUI.java
+++ Demo 3/src/visuals/MenuGUI.java
@@ -1,122 +1,111 @@
 package visuals;
+/**
+ * This class is used to display the GUI menu screen
+ * 
+ * @author Group 7, adapted from Almas Baimagambetov: https://www.youtube.com/
+	 watch?v=l2XhUHW8Oa4&list=PLurZmf6mNWh4oNzAph6vk14xj9NdS-RCP&index=2&t=0s
+ * @version 1.0
+ * @since 2019-03-21
+ */
 
-import javafx.geometry.Pos;
-import javafx.scene.control.*;
-import javafx.scene.effect.DropShadow;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.*;
 import logic.*;
 import drivers.*;
 import handlers.*;
 
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
-import java.sql.SQLOutput;
-
-public class MenuGUI {
+public class MenuGUI extends MainGUI{
 
     private Pane root = new Pane();
-    private Game game;
-
+	
+    /**
+	 * Starts the javafx application
+	 * @param Stage stage
+	 */
     public void start(Stage stage){
-        game = new Game();
 
-        Scene gameScene = new Scene(game.getVisual().getRoot(), MainGUI.WIDTH, MainGUI.HEIGHT);
+        Game game = new Game();
+
+        game.start();
+
+        Scene gameScene = new Scene(game.getVisual().getRoot(), WIDTH, HEIGHT);
         gameScene.setOnKeyPressed(new PressHandler());
         gameScene.setOnKeyReleased(new ReleaseHandler());
 
-        createMenu(stage, gameScene);
-        Scene menuScene = new Scene(root, MainGUI.WIDTH, MainGUI.HEIGHT);
+        menu(stage, gameScene);
+
+        Scene menuScene = new Scene(root, WIDTH, HEIGHT);
         stage.setScene(menuScene);
     }
 
-    private void createMenu(Stage stage, Scene gameScene){
-        root.getChildren().add(createTitle());
-        root.getChildren().add(createStartBtn(stage, gameScene));
-        root.getChildren().add(createMapBtnBox(game));
-        root.getChildren().add(createCredits());
-        root.setBackground(createBackground());
-    }
-
-    private Label createTitle() {
+	/**
+	 * Creation of the menu screen
+	 * @param Stage stage, Scene gameScene
+	 */
+    public void menu(Stage stage, Scene gameScene){
+		//Sets the background image
+        Image tank = new Image("tanks.jpg");
+        ImageView mv = new ImageView();
+        mv.setImage(tank);
+	
+		//Creates the title 
         Label titleLbl = new Label("Tank Royale");
+        //titleLbl.setFont(Font.loadFont("file:resources/fonts/ToetheLineless.ttf", 50));
         titleLbl.setFont(Font.loadFont(getClass().getResourceAsStream("/resources/fonts/ToetheLineless.ttf"), 50));
         titleLbl.setPrefWidth(400);
         titleLbl.setLayoutX(545 - titleLbl.getWidth() - 157);
         titleLbl.setLayoutY(125);
-        return titleLbl;
-    }
+        root.getChildren().add(titleLbl);
 
-    private Button createStartBtn(Stage stage, Scene gameScene) {
+		//Creation of the start button 
         Button startBtn = new Button("Start");
-        startBtn.setStyle("-fx-background-color: #991E1E; -fx-font-size: 2em; -fx-text-fill: #ffffff;" +
-                            " -fx-border-color: #ff0000; -fx-border-width: 2px; ");
+        startBtn.setStyle("-fx-background-color: #991E1E; -fx-font-size: 2em; -fx-text-fill: #ffffff; -fx-border-color: #ff0000; -fx-border-width: 2px; ");
         startBtn.setPrefWidth(200);
         startBtn.setPrefHeight(45);
-        startBtn.setLayoutX(MainGUI.WIDTH / 2.0 - startBtn.getPrefWidth()/2.0);
+        startBtn.setLayoutX(545 - startBtn.getWidth() - 90);
         startBtn.setLayoutY(225);
+        root.getChildren().add(startBtn);
 
-        DropShadow shadow = new DropShadow();
-        startBtn.addEventHandler(MouseEvent.MOUSE_ENTERED, e -> startBtn.setEffect(shadow));
-        //Removing the shadow when the mouse cursor is off
-        startBtn.addEventHandler(MouseEvent.MOUSE_EXITED, e -> startBtn.setEffect(null));
-        startBtn.setOnAction(e -> {
-            stage.setScene(gameScene);
-            game.start();
-        });
+		//Creation of the select map button
+        Button selectMapBtn = new Button("Select Map");
+        selectMapBtn.setPrefWidth(200);
+        selectMapBtn.setLayoutX(545 - selectMapBtn.getWidth() - 90);
+        selectMapBtn.setLayoutY(290);
+        root.getChildren().add(selectMapBtn);
 
-        return startBtn;
-    }
-
-    public static ToggleButton createMapBtn(String name, String mapFilePath, Game game) {
-        ToggleButton selectMapBtn = new ToggleButton(name);
-        selectMapBtn.setPrefWidth(100);
-
-        selectMapBtn.setOnAction(e -> {
-            game.setMapFilePath(mapFilePath);
-        });
-        return selectMapBtn;
-    }
-
-    public static HBox createMapBtnBox(Game game){
-        int mapCount = 3;
-        ToggleButton mapBtn1 = createMapBtn("Maze", "/resources/GUI/maze.txt", game);
-        ToggleButton mapBtn2 = createMapBtn("Empty", "/resources/GUI/empty.txt", game);
-        ToggleButton mapBtn3 = createMapBtn("Test", "/resources/GUI/heart.txt", game);
-
-        final ToggleGroup group = new ToggleGroup();
-        mapBtn1.setToggleGroup(group);
-        mapBtn2.setToggleGroup(group);
-        mapBtn3.setToggleGroup(group);
-        mapBtn1.setSelected(true);
-
-        HBox hbox = new HBox(mapBtn1, mapBtn2, mapBtn3);
-        hbox.setLayoutX(MainGUI.WIDTH / 2.0 - mapBtn1.getPrefWidth()*mapCount / 2.0);
-        hbox.setLayoutY(MainGUI.HEIGHT/2.0 + 5);
-        return hbox;
-    }
-
-
-    private Label createCredits() {
+		//Creation of the credits text
         Label credits = new Label("Created By: Anjola Adeboye, Harry Chen, Mei Hou, Josh Kim and Andre Staffa");
+        //credits.setFont(Font.loadFont("file:resources/fonts/RiseofKingdom.ttf", 15));
         credits.setFont(Font.loadFont(MenuGUI.class.getResourceAsStream("/resources/fonts/RiseofKingdom.ttf"), 15));
         credits.setTextFill(Color.WHITE);
         credits.setLayoutX(12);
         credits.setLayoutY(575);
-        return credits;
-    }
+        root.getChildren().add(credits);
 
-    private Background createBackground() {
+		//Event handler for the start button to start the game
+        startBtn.setOnAction(e -> {
+            stage.setScene(gameScene);
+        });
+
+		//Future Event handler for the Map selection screen
+        selectMapBtn.setOnAction(e -> {
+            System.out.println("Select Map Button Selected");
+        });
+
         BackgroundImage backdrop = new BackgroundImage(new Image(
                 MenuGUI.class.getResourceAsStream("/resources/images/tanks.jpg"),
                 1080,680,false,true),
                 BackgroundRepeat.REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT,
                 BackgroundSize.DEFAULT);
-        return new Background(backdrop);
+        root.setBackground(new Background(backdrop));
     }
 
 }
