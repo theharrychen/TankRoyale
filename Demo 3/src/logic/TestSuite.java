535b26b1fa270c2e74b15c6e4fd907536a7d941f
Harry <harry.chen1@ucalgary.ca>
2019-03-28 03:55:34 -0600

    working testsuite

---
+++ Demo 3/src/logic/TestSuite.java
@@ -0,0 +1,16 @@
+package logic;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        MapTest.class, //test case 1
+        WallTest.class     //test case 2
+})
+public class TestSuite {
+    //normally, this is an empty class
+}
