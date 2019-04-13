8318c85dd9fdc3df8738d6b88b72ebd5c4ed1e13
AnjolaAA <45274850+AnjolaAA@users.noreply.github.com>
2019-04-12 18:37:38 -0600

    Update GameEntity.java

--- Demo 3/src/logic/GameEntity.java
+++ Demo 3/src/logic/GameEntity.java
@@ -14,39 +14,6 @@ import javafx.scene.Node;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.transform.Rotate;
 import javafx.geometry.Point2D;
-
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
-import java.awt.geom.AffineTransform;
-
-import javafx.scene.shape.Shape;
-
-
-public class GameEntity { // By default a physically "static" object
-
-    private Node view; //An item in the scene graph 
-    private boolean alive = true;
-    private int x, y; //Variables for text based version
-
-    //Contructor: Creates a GameEntity for GameGUI version of the game
-    public GameEntity(Node view) {
-        setView(view);package logic;
-
-/**
- * This class is used to generate a bullet or project to be shot by the tank.
- * The class extends GameEntity which tracks the bullets x and y coordinates in
- * the generated grid/array.
- *
- * @author Team 7
- * @version 1.0
- * @since 2019-02-19
- */
-
-import javafx.scene.Node;
-import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.transform.Rotate;
-import javafx.geometry.Point2D;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -86,7 +53,7 @@ public class GameEntity { // By default a physically "static" object
 	 */
 	public void setX(int x) {
     if(x < 0){
-      new SafeCrash("Error: invalid value.", "X-coordinate cannot be negative." "Logic, GameEntity, SetX");
+      //S AFE CRASH
     }
 		this.x = x;
 	}
@@ -104,7 +71,7 @@ public class GameEntity { // By default a physically "static" object
 	 */
 	public void setY(int y) {
     if(y < 0){
-      new SafeCrash("Error: invalid value.", "Y-coordinate cannot be negative." "Logic, GameEntity, SetY");
+      //S AFE CRASH
     }
 		this.y = y;
 	}
@@ -157,93 +124,6 @@ public class GameEntity { // By default a physically "static" object
 		return collisionDetected;
 	}
 
-
-
-}
-
-    }
-
-    //Constructor for text based version of the game
-    public GameEntity(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
-
-    /**
-     * @return int x
-     */
-    public int getX() {
-        return x;
-    }
-
-    /**
-     * sets the x coordinate
-     */
-    public void setX(int x) {
-        this.x = x;
-    }
-
-    /**
-     * @return int y
-     */
-    public int getY() {
-        return y;
-    }
-
-    /**
-     * sets the y coordinate
-     */
-    public void setY(int y) {
-        this.y = y;
-    }
-
-    /**
-     *Updates the game Node to the new position
-     *@param view: intended new view
-     */
-    public void setView(Node view) {
-        this.view = view;
-    }
-
-    //Returns the Node (possible privacy leak)
-    public Node getView() {
-        return view;
-    }
-
-    public double getRotation() {
-        return this.getView().getRotate();
-    }
-
-    /**
-     *Updates current state to alive.
-     *@param alive: new intended state
-     */
-    public void setAlive(boolean alive) {
-        this.alive = alive;
-    }
-
-    //Setting the state to alive
-    public boolean isAlive() {
-        return alive;
-    }
-
-    //Returns false if static is dead 
-    public boolean isDead() {
-        return !alive;
-    }
-
-    //Checks if any two objects are at the same position -> therefore colliding. 
-    // This is adapted from https://stackoverflow.com/questions/15013913/checking-collision-of-shapes-with-javafx
-    public boolean isColliding(GameEntity other) {
-        boolean collisionDetected = false;
-        Shape object1 = (Shape) this.getView();
-        Shape object2 = (Shape) other.getView();
-        Shape intersect = Shape.intersect(object1, object2);
-        if (intersect.getBoundsInLocal().getWidth() != -1) {
-            collisionDetected = true;
-        }
-        return collisionDetected;
-    }
 
 
 }
